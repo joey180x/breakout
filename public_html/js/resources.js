267e30ed19ff364e10beadf81ddcc549c1f4b72a
joey180x <joey180x@icloud.com>
2014-03-04 08:28:37 -0800

    Initial commit
    Initial commit of the Breakout melonJs game created for AppAcademy

---
+++ public_html/js/resources.js
@@ -0,0 +1,45 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+          
+        // *** App Academy ***
+        // Add two graphics files, tiles and paddle, as a resource to use
+        {name: "tiles",type: "image", src: "data/img/tiles.png"},
+        {name: "paddle", type: "image", src: "data/img/paddle.png"},
+        {name: "ball", type: "image", src: "data/img/ball.png"},
+        {name: "brick", type: "image", src: "data/img/brick.png"},
+        {name: "solid-tile", type: "image", src: "data/img/solid-tile.png"},
+        
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 O*/
+        {name: "level01", type: "tmx", src: "data/map/level01.tmx"},
+        {name: "level02", type: "tmx", src: "data/map/level02.tmx"}
+        
+        
+        
+       
+        // *** App Academy ***
+        // Add the level as a resource to use within the program
+
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
+	 */
+];
---
+++ public_html/js/screens/play.js
@@ -0,0 +1,31 @@
+game.PlayScreen = me.ScreenObject.extend({
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {
+		// reset the score
+		game.data.score = 0;
+                
+                
+                // *** App Academy ***
+                // Load the level you created in the program Tiled
+                me.levelDirector.loadLevel("level01");
+                
+                var ball = new game.BallEntity(50, 50, {});
+                me.game.add(ball, this.z);
+                    
+
+		// add our HUD to the game world
+		this.HUD = new game.HUD.Container();
+		me.game.world.addChild(this.HUD);
+	},
+
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		// remove the HUD from the game world
+		me.game.world.removeChild(this.HUD);
+        }
+});
